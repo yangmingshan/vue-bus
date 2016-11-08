cb7aeec74ed44320a4a284e6f9213bc3af9f2d72
yangmingshan <yang@yangmingshandeMac-mini.local>
2016-11-08 23:15:37 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # vue-bus
 A event bus for Vue.js
+
+## Installation
+```
+$ npm install vue-resource
+```
+When used with a module system, you must explicitly install the router via Vue.use():
+```
+import Vue from 'vue';
+import VueBus from 'vue-bus';
+
+Vue.use(VueBus);
+```
+You don't need to do this when using global script tags.
+
+## Usage
+#### Listen to an event
+```
+new Vue({
+  created() {
+    this.$bus.on('event', data => console.log(data));
+  }
+});
+```
+#### Trigger an event
+```
+new Vue({
+  methods: {
+    test() {
+      this.$bus.emit('event', 'test');
+    }
+  }
+});
+```
+
+## License
+[MIT](http://opensource.org/licenses/MIT)
---
+++ index.js
@@ -0,0 +1,42 @@
+/**
+ * vue-bus v0.1.0
+ * https://github.com/yangmingshan/vue-bus
+ * @license MIT
+ */
+(function(global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.VueBus = factory());
+}(this, (function() { 'use strict';
+
+  function VueBus(Vue) {
+
+    var bus = new Vue();
+
+    Object.defineProperties(bus, {
+      on: {
+        get: function() {
+          return this.$on;
+        }
+      },
+      emit: {
+        get: function() {
+          return this.$emit;
+        }
+      }
+    });
+
+    Object.defineProperty(Vue.prototype, '$bus', {
+      get: function() {
+        return bus;
+      }
+    });
+  }
+
+  if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(VueBus);
+  }
+
+  return VueBus;
+
+})));
